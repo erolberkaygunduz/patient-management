49d2628b25e140d265055a22d6a76f9b94dec6f0
erolberkaygunduz <erolberkaygunduz@gmail.com>
2026-08-21 00:22:36 +0200

    .

---
+++ auth-service/src/test/java/net/berkaygunduz/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.berkaygunduz.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
